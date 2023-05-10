77f9223f22d683f43f2386c3ab403217663017ec
Joery <j.van.der.meijden@outlook.com>
2023-05-10 16:09:55 +0200

    Added data split function and train and test files of the original data file

---
+++ split_train_test_data.py
@@ -0,0 +1,34 @@
+import math
+import pandas as pd
+
+# TODO only needs to be run once!!
+
+# Constants
+train_percentage = 0.8  # Percentage of train data as a decimal number, test data is 1 - train_percentage
+
+
+def split_train_test_data(train_percentagex):
+    # Import df from csv - already sorted on timestamp so no need to sort
+    df = pd.read_csv("burglary_data.csv")
+
+    # Create index number to split on
+    split_border = len(df) * train_percentagex
+
+    # Split df into train and test data set
+    df_training = df[:math.floor(split_border)]
+    df_test = df[math.ceil(split_border):]
+
+    # Remove duplicated data
+    # Should not be the case here but have to double check
+
+    # Convert df training to csv
+    df_training.to_csv('burglary_train.csv')
+
+    # Convert df test to csv
+    df_test.to_csv('burglary_test.csv')
+
+    print("Train len: ", len(df_training))
+    print("Test len: ", len(df_test))
+
+
+split_train_test_data(train_percentage)
