673abb32227f1b8ea4e547a6eddf800c7c66fa67
Joery <j.van.der.meijden@outlook.com>
2023-05-12 12:27:26 +0200

    Update split_train_test_data.py

--- split_train_test_data.py
+++ split_train_test_data.py
@@ -4,17 +4,17 @@ import pandas as pd
 # TODO only needs to be run once!!
 
 # Constants - the sum of all should be 1
-train_percentage = 0.7
-validation_percentage = 0.1
-test_percentage = 0.2
+train = 0.7
+validation = 0.1
+test = 0.2  # Test percentage is 1 - train_percentage - validation_percentage
 
 
-def split_train_test_data(train_percentagex):
+def split_train_test_data(train_percentage, validation_percentage):
     # Import df from csv - already sorted on timestamp so no need to sort
     df = pd.read_csv("burglary_data.csv")
 
     # Create index number to split on
-    split_border_train_valid = len(df) * train_percentagex
+    split_border_train_valid = len(df) * train_percentage
     split_border_valid_test = len(df) * validation_percentage
 
     # Split df into train and test data set
@@ -39,4 +39,4 @@ def split_train_test_data(train_percentagex):
     print("Test len: ", len(df_test))
 
 
-split_train_test_data(train_percentage)
+split_train_test_data(train, validation)
