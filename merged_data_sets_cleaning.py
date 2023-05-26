9dcb2a64dbfb5bca8b64d3fa38650c04e23da001
Joery <j.van.der.meijden@outlook.com>
2023-05-26 22:10:21 +0200

    Fixed final dataset
    The dataset to be used for the training of models is Final_dataset.csv

--- merged_data_sets_cleaning.py
+++ merged_data_sets_cleaning.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 
-def clean_file():
+def clean_merged_file():
     df_ward_profiles = pd.read_csv("ward_median_house_merged.csv", sep=",", index_col=0)
 
-    df_ward_profiles.drop(columns=["% English is First Language of no one in household - 2011"])
+    df_ward_profiles = df_ward_profiles[["Ward name", "New code", "% All Working-age (16-64) - 2015", "% All Older people aged 65+ - 2015",
+                                        "Population density (persons per sq km) - 2013", "% Not Born in UK - 2011", "Below 33%", "Above 66%"]]
 
     df_ward_profiles.to_csv("ward_median_house_merged_cleaned.csv", index=False)
 
@@ -24,3 +25,13 @@ def clean_LSOA_to_Ward():
     df_LSOA_Ward.to_csv("LSOA_name_and_Ward_codes.csv", index=False)
 
 
+def clean_final_merged_set():
+    df = pd.read_csv("LSOA_name_ward_median_house.csv")
+    df["% All Working-age (16-64) - 2015"] = df["% All Working-age (16-64) - 2015"]/10
+    df["% All Older people aged 65+ - 2015"] = df["% All Older people aged 65+ - 2015"] / 10
+    df["% Not Born in UK - 2011"] = df["% Not Born in UK - 2011"] / 10
+
+    df = df.drop(columns=["New code","Crime ID", "Reported by", "Crime type"])
+
+    print(df.head())
+    df.to_csv("Final_dataset.csv", index=False)
